4ce16ed74e2f8d67398ed884983c6d469191d992
Tushar <tusharkhandelwal1512@gmail.com>
2023-10-02 21:10:47 +0530

    corrected spelling

--- src/studentDashboard/SubjectDisplayCard.jsx
+++ src/studentDashboard/SubjectDisplayCard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
-import { capitalizeFirstLetter } from "../subjectDashboard/subjectDashboard";
+import { capitalizeFirstLetter } from "../subjectDashboard/SubjectDashboard";
 
 
 const SubjectDisplayCard = (props) => {
